279b9e87c89bce89863ad530a49d235793d4be60
Gabriel <gabriel@debian>
2023-05-13 00:58:04 -0300

    WSGI simple server and response.

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-print("Hello world")
+import wsgiref.simple_server
+
+def application(environ, start_response):
+    # response
+    response = b"Hello world"
+    # status of response
+    status = "200 OK"
+    # response header
+    headers = [("Content-Type", "text/html")]
+    # setting status and headers
+    start_response(status, headers)
+
+    return [response]
+
+if __name__ == "__main__":
+    w_s = wsgiref.simple_server.make_server(
+        host="localhost",
+        port=8024,
+        app=application
+    )
+    w_s.handle_request()
